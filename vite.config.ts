a6da905be8ec18fc3e4a0c897619c614b3465c01
Garvit Soni <136806468+SoniGarvit@users.noreply.github.com>
2025-11-06 20:36:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/calendar-view-garvit/',
+  base:'/calendar-view/',
   plugins: [react()],
 })
